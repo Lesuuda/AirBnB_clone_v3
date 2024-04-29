02affd1ff4eaa762a9eae7334917fcb11d36a05c
lesuuda <lesuudaljamani@gmail.com>
2024-04-29 09:23:07 +0300

    core: added function to return stats of objects in the db

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
-
+from models import storage
 
 app = Flask(__name__)
 
@@ -13,3 +13,16 @@ app = Flask(__name__)
 def status():
     """return the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns the stats of all the objects in the db"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
