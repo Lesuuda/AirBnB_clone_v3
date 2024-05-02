467a96d5306646416a045d89004c4e5f31984218
tonui14 <tonuidaisy@gmail.com>
2024-05-02 18:17:21 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
+
 """
 starts the flask application
 """
 
-
 import os
 from flask import Flask, jsonify
+from werkzeug.exceptions import NotFound
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-
 
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def close_storage(exception):
-    """Close the current SQLAlchemy session."""
-    storage.close()
-
+def teardown_appcontext(exception):
+    """Closes the storage"""
+    storage.close() 
 
 @app.errorhandler(404)
 def page_not_found(exception):
     """Return a JSON-formatted 404 page."""
     return jsonify({"error": "Not found"}), 404
-
-
+    
+    
 if __name__ == "__main__":
     """runs the flask server"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True, debug=True)
-
